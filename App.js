c6f31c585a87739769d8b325e9ebd9da77275e02
jacobitkashala <jacobitkashala@gmail.com>
2021-07-16 14:29:16 +0200

    ajout: amelioration

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Seaech from './component/Search';
-import Film from './component/FilmItem';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+//import { StyleSheet, Text, View } from 'react-native';
+import Search from './component/Search';
+//import Film from './component/FilmItem';
+//import Navigation from './Navigation/navigation';
 
+const Stack = createNativeStackNavigator();
+const stack = createStackNavigator()
 export default function App() {
   return (
-    <>
-      <Seaech />
-    </>
+    <NavigationContainer>
+      <stack.Navigator>
+        <stack.Screen
+          name="search"
+          component={Search}
+        />
+      </stack.Navigator>
+    </NavigationContainer>
+
+
   )
 }
 
