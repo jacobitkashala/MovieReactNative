7c420f33412d4c2da1cfb5dee257131fac0a38dc
jacobitkashala <jacobitkashala@gmail.com>
2021-07-19 10:06:32 +0200

    add:selectionner un film lors du click

--- API/TMD.js
+++ API/TMD.js
@@ -2,8 +2,6 @@
 import { API_TOKEN } from "../.env"
 
 export function getFilmsFromApiWithSearchedText(text, page) {
-  //&page=${1}
-  //console.log(API_TOKEN)
   const url = "https://api.themoviedb.org/3/search/movie?api_key=" + API_TOKEN + "&language=fr&query=" + text + "&page=" + page
 
   return fetch(url)
--- App.js
+++ App.js
@@ -1,19 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-//import { NavigationContainer } from '@react-navigation/native';
-//import { createStackNavigator } from '@react-navigation/stack';
-//import { StyleSheet, Text, View } from 'react-native';
-import Search from './component/Search';
-//import Film from './component/FilmItem';
-//import Navigation from './Navigation/navigation';
+import { createStackNavigator } from '@react-navigation/stack';
+//import { Text, View, Button } from 'react-native';
+import Home from './components/Search';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import Detail from './components/DetailFilm';
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//       <Button
+//         title="Go to Details"
+//         onPress={() => navigation.navigate('Details')}
+//       />
+//     </View>
+//   );
+// }
+// function DetailsScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center' }}>
+//       <Text>Details Screen</Text>
+//     </View>
+//   );
+// }
 
-// const Stack = createNativeStackNavigator();
-// const stack = createStackNavigator()
-export default function App() {
-  
+const Stack = createStackNavigator();
+// function app2() {
+
+//   return (
+//     // <Search />
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Accueil" component={HomeScreen} />
+//         <Stack.Screen name="Details" component={DetailsScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   )
+// }
+export default function app() {
+
   return (
-    <Search />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Accueil" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
-
 
