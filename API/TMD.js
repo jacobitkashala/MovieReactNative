00c3c9418046047b485f58e4250075dc37e46aad
jacobitkashala <jacobitkashala@gmail.com>
2021-07-19 12:56:20 +0200

    add:affiche les info detail de movie

--- API/TMD.js
+++ API/TMD.js
@@ -9,9 +9,15 @@ export function getFilmsFromApiWithSearchedText(text, page) {
     .catch((error) => console.log("error:", error));
 
 }
-
 export function getImageFromApi(name) {
-
   return 'https://image.tmdb.org/t/p/w300' + name
+}
 
+export function getFilmDetailFromApi(id) {
+  //https://api.themoviedb.org/3/movie/343611?api_key=c8697268acc5406f1d3c61343bbfd606&language=fr
+  const url = "https://api.themoviedb.org/3/movie/" + id + "?api_key=" + API_TOKEN + "&language=fr";
+
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.log("error:", error));
 }
